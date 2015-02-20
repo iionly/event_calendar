414251bc7858eea34c7aade505be3a57d5e83ead
iionly <iionly@gmx.de>
2015-02-20 19:45:07 +0100

    Cleanup of input views folder

--- views/default/input/timepicker.php
+++ views/default/input/timepicker.php
@@ -1,4 +1,5 @@
 <?php
+
 $time_format = elgg_get_plugin_setting('timeformat', 'event_calendar');
 if (!$time_format) {
 	$time_format = '24';
@@ -6,44 +7,44 @@ if (!$time_format) {
 
 $value = $vars['value'];
 if (isset($vars['hours']) && $vars['hours']) {
-  $hour = $vars['hours'];
-  $minute = $vars['minutes'];
-  $meridian = $vars['meridian'];
+	$hour = $vars['hours'];
+	$minute = $vars['minutes'];
+	$meridian = $vars['meridian'];
 } else {
-  if (is_numeric($value)) {
-  	$hour = floor($value/60);
-  	$minute = ($value -60*$hour);
-  } else {
-  	$hour = 0;
-  	$minute = 0;
-  }
-  if ($time_format == '12') {
-    if ($hour == 0) {
-      $hour = 12;
-      $meridian = 'am';
-    } else if ($hour == 12) {
-      $meridian = 'pm';
-    } else if ($hour < 12) {
-      $meridian = 'am';
-    } else {
-      $hour -= 12;
-      $meridian = 'pm';
-    }
-  }
+	if (is_numeric($value)) {
+	$hour = floor($value/60);
+	$minute = ($value -60*$hour);
+	} else {
+		$hour = 0;
+		$minute = 0;
+	}
+	if ($time_format == '12') {
+		if ($hour == 0) {
+			$hour = 12;
+			$meridian = 'am';
+		} else if ($hour == 12) {
+			$meridian = 'pm';
+		} else if ($hour < 12) {
+			$meridian = 'am';
+		} else {
+			$hour -= 12;
+			$meridian = 'pm';
+		}
+	}
 }
 
 $hours = array();
 $minutes = array();
 
 if ($time_format == '12') {
-  $meridians = array('am'=>'am','pm'=>'pm');
-  for($h=1;$h<=12;$h++) {
-    $hours[$h] = $h;
-  }
+	$meridians = array('am'=>'am','pm'=>'pm');
+	for($h=1;$h<=12;$h++) {
+		$hours[$h] = $h;
+	}
 } else {
-  for($h=0;$h<=23;$h++) {
-    $hours[$h] = $h;
-  }
+	for($h=0;$h<=23;$h++) {
+		$hours[$h] = $h;
+	}
 }
 
 for($m=0;$m<60;$m=$m+5) {
@@ -51,9 +52,9 @@ for($m=0;$m<60;$m=$m+5) {
 	$minutes[$m] = $mt;
 }
 
-echo elgg_view('input/dropdown',array('name'=>$vars['name'].'_hour','value'=>$hour,'options_values'=>$hours));
+echo elgg_view('input/dropdown', array('name' => $vars['name'].'_hour', 'value' => $hour, 'options_values' => $hours));
 echo " <b>:</b> ";
-echo elgg_view('input/dropdown',array('name'=>$vars['name'].'_minute','value'=>$minute,'options_values'=>$minutes));
+echo elgg_view('input/dropdown', array('name' => $vars['name'].'_minute', 'value' => $minute, 'options_values' => $minutes));
 if ($time_format == '12') {
-	echo elgg_view('input/dropdown',array('name'=>$vars['name'].'_meridian','value'=>$meridian,'options_values'=>$meridians));
+	echo elgg_view('input/dropdown', array('name' => $vars['name'].'_meridian', 'value' => $meridian, 'options_values' => $meridians));
 }
