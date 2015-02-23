cedca0d3c72458733edfad8209a51c38d410883b
iionly <iionly@gmx.de>
2015-02-23 23:14:42 +0100

    No longer use private API function generate_action_token

--- views/default/event_calendar/personal_toggle_js.php
+++ views/default/event_calendar/personal_toggle_js.php
@@ -2,21 +2,19 @@
 
 // TODO: put the rest into a JS function
 
-$elgg_ts = time();
-$elgg_token = generate_action_token($elgg_ts);
-$tokens = "&__elgg_ts=$elgg_ts&__elgg_token=$elgg_token";
 ?>
 
 <script type="text/javascript">
 function event_calendar_personal_toggle(event_id, user_id) {
-
-	var link = "<?php echo elgg_get_site_url(); ?>action/event_calendar/toggle_personal_calendar?";
-	link += "user_id="+user_id+"&event_id="+event_id+"&other=true";
-	link += "<?php echo $tokens; ?>";
-	$.get(link,
-		function (res) {
+	elgg.action('event_calendar/toggle_personal_calendar', {
+		data: {
+			user_id: user_id,
+			event_id: event_id,
+			other: true
+		},
+		success: function(res) {
 			$('#event_calendar_user_data_'+user_id).html(res);
 		}
-	);
+	});
 }
 </script>
