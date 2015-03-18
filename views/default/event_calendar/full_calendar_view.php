3f69d2572ab0bc06160ae5af084432c0be0beee1
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2015-03-18 15:38:11 +0200

    Fixes opening of an event
    This got broken when events were modified not to get opened inside a
    lightbox. Now the event details get opened correctly into a new page.

--- views/default/event_calendar/full_calendar_view.php
+++ views/default/event_calendar/full_calendar_view.php
@@ -11,12 +11,7 @@ var goToDateFlag = 0;
 
 handleEventClick = function(event) {
 	if (event.url) {
-		if (event.is_event_poll) {
-        	window.location.href = event.url;
-		} else {
-			//window.location.href = event.url;
-			$.fancybox({'href':event.url});
-		}
+		window.location.href = event.url;
 		return false;
 	}
 };
