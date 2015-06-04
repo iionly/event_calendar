9daa7d94c7e6b5ff9923fa9a1df733014f681941
iionly <iionly@gmx.de>
2015-06-04 16:40:04 +0200

    Toggles between Add to/remove from calendar buttons when clicked

--- actions/event_calendar/toggle_personal_calendar.php
+++ actions/event_calendar/toggle_personal_calendar.php
@@ -17,9 +17,8 @@ if ($other) {
 }
 
 if (event_calendar_has_personal_event($event_guid, $user_guid)) {
-	$button_text = elgg_echo('event_calendar:add_to_the_calendar');
 	event_calendar_remove_personal_event($event_guid, $user_guid);
-	$response = array('success' => true, 'message' => $remove_response, 'button_text' => $button_text, 'button_class' => 'elgg-button elgg-button-submit event-calendar-personal-calendar-toggle');
+	$response = array('success' => true, 'message' => $remove_response);
 } else {
 	if (event_calendar_add_personal_event($event_guid, $user_guid)) {
 		$event_calendar_add_users_notify = elgg_get_plugin_setting('add_users_notify', 'event_calendar');
@@ -36,8 +35,7 @@ if (event_calendar_has_personal_event($event_guid, $user_guid)) {
 				));
 			}
 		}
-		$button_text = elgg_echo('event_calendar:remove_from_the_calendar_button');
-		$response = array('success' => true, 'message' => $add_response, 'button_text' => $button_text, 'button_class' => 'elgg-button elgg-button-delete event-calendar-personal-calendar-toggle');
+		$response = array('success' => true, 'message' => $add_response);
 	} else {
 		$response = array('success' => false, 'message' => $add_error);
 	}
--- views/default/js/event_calendar/event_calendar.php
+++ views/default/js/event_calendar/event_calendar.php
@@ -3,7 +3,7 @@ elgg.provide('elgg.event_calendar');
 
 elgg.event_calendar.init = function () {
 	$('.event_calendar_paged_checkbox').click(elgg.event_calendar.handlePagedPersonalCalendarToggle);
-	$('.event-calendar-personal-calendar-toggle').click(elgg.event_calendar.handleDisplayPagePersonalCalendarToggle);
+	$('.event-calendar-personal-calendar-toggle').click(elgg.event_calendar.toggleDisplayPagePersonalCalendar);
 	$('#event-calendar-region').change(elgg.event_calendar.handleRegionChange);
 	$('#event-calendar-ical-link').click(elgg.event_calendar.handleIcalPopup);
 	$('.event-calendar-repeating-unselected').each(elgg.event_calendar.setRepeatingClass);
@@ -109,6 +109,7 @@ elgg.event_calendar.handlePagedPersonalCalendarToggle = function() {
 	guid = parseInt($(this).attr('id').substring('event_calendar_paged_checkbox_'.length));
 	elgg.event_calendar.togglePagedPersonalCalendar(guid);
 }
+
 elgg.event_calendar.togglePagedPersonalCalendar = function(guid) {
 	elgg.action('event_calendar/toggle_personal_calendar',
 			{
@@ -131,33 +132,27 @@ elgg.event_calendar.togglePagedPersonalCalendar = function(guid) {
 	);
 }
 
-elgg.event_calendar.handleDisplayPagePersonalCalendarToggle = function() {
-	var guidBit = $(this).attr('id').substring('event_calendar_user_data_'.length);
-	var guids = guidBit.split('_');
-	var event_guid = parseInt(guids[0]);
-	var user_guid = parseInt(guids[1]);
-	elgg.event_calendar.toggleDisplayPagePersonalCalendar(event_guid,user_guid);
-}
+/**
+ * Add/Remove an event from users personal calendar
+ */
+elgg.event_calendar.toggleDisplayPagePersonalCalendar = function() {
+	var event_guid = $(this).attr('data-event-guid');
+	var user_guid = $(this).attr('data-user-guid');
 
-elgg.event_calendar.toggleDisplayPagePersonalCalendar = function(event_guid,user_guid) {
-	elgg.action('event_calendar/toggle_personal_calendar',
-			{
-				data: {event_guid: event_guid,user_guid: user_guid, other: 'yes'},
-				success: function (res) {
-							var success = res.success;
-							var msg = res.message;
-							if (success) {
-								var button_text = res.button_text;
-								var button_class = res.button_class;
-								$('#event_calendar_user_data_'+event_guid+'_'+user_guid).val(button_text);
-								$('#event_calendar_user_data_'+event_guid+'_'+user_guid).attr('class', button_class);
-								//elgg.system_message(msg,2000);
-							} else {
-								elgg.register_error(msg,2000);
-							}
-					    }
+	elgg.action('event_calendar/toggle_personal_calendar', {
+		data: {
+			event_guid: event_guid,
+			user_guid: user_guid,
+			other: 'yes'
+		},
+		success: function (json) {
+			if (json.success) {
+				$('[data-user-guid=' + user_guid + ']').toggle();
+			} else {
+				elgg.register_error(json.message, 2000);
 			}
-	);
+	    }
+	});
 }
 
 elgg.register_hook_handler('init', 'system', elgg.event_calendar.init);
