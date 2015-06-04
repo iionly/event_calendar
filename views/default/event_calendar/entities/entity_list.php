fa7552514087b084627f723be3c84982c4d8f155
iionly <iionly@gmx.de>
2015-06-04 22:40:18 +0200

    Cleanup of entity_list view (no longer use deprecated navigation/viewtype view that were unnecessary to be used anyway)

--- views/default/event_calendar/entities/entity_list.php
+++ views/default/event_calendar/entities/entity_list.php
@@ -9,34 +9,16 @@
  *
  */
 
-$context = $vars['context'];
 $offset = $vars['offset'];
 $entities = $vars['entities'];
 $limit = $vars['limit'];
 $count = $vars['count'];
 $base_url = $vars['base_url'];
-$context = $vars['context'];
-$viewtype = $vars['viewtype'];
 $pagination = $vars['pagination'];
 $full_view = $vars['full_view'];
 
 $html = "";
 $nav = "";
-
-if (isset($vars['list_type_toggle'])) {
-	$list_type_toggle = $vars['list_type_toggle'];
-} else {
-	$list_type_toggle = true;
-}
-
-if ($context == "search" && $count > 0 && $list_type_toggle) {
-	$nav .= elgg_view('navigation/viewtype', array(
-		'base_url' => $base_url,
-		'offset' => $offset,
-		'count' => $count,
-		'viewtype' => $viewtype,
-	));
-}
 
 if ($pagination) {
 	$nav .= elgg_view('navigation/pagination', array(
